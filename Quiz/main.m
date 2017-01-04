560dbe062355b0c77bf3f9133aec0268ac065a73
crushbowl <crushbowl@users.noreply.github.com>
2017-01-04 12:55:14 -0500

    Add files via upload

---
+++ Quiz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Hodge, Janelle on 1/4/17.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
